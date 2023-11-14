df925e837708ef7a187b939ee7ea7bab834f634d
saadat <saadat0917@gmail.com>
2023-11-14 11:09:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 import './App.css';
 
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [jobIndex, setJobIndex] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  // loading screen
+  if(loading) {
+    return (
+      <section className='loading'>
+        <h2>loading</h2>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[jobIndex];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='job-btn-container'>
+          {jobs.map((item, index) => {
+              return (
+                <button className={`job-btn ${index === jobIndex && 'active-btn'}`} 
+                        key={item.id} 
+                        onClick={() => setJobIndex(index)}
+                >
+                 {item.company}
+                </button>
+              );            
+            })} 
+        </div>
+        {/* jobs info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-description'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button 
+        type="button" 
+        className="info-btn"
+        onClick={() => window.location.href='https://en.wikipedia.org/wiki/Rickrolling'}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        more info
+      </button>
+    </section>
   );
-}
+};
 
 export default App;
